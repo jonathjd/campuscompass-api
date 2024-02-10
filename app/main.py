d49e04d17f0720813f63e6a8e58939d74b485eec
Jonathan Dickinson <jon.dickinson17@gmail.com>
2024-02-10 08:04:04 -0800

    Add 8000 port to dockerfile

--- app/main.py
+++ app/main.py
@@ -3,20 +3,11 @@ from fastapi import FastAPI
 
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
-from functools import lru_cache
-from app import settings
 from app.routers import locations
 from app.dependencies.dependencies import limiter
 
 
-@lru_cache
-def get_settings():
-    return settings.Settings()
-
-
-config = get_settings()
-
-app = FastAPI(title=config.app_name)
+app = FastAPI()
 
 
 app.state.limiter = limiter
